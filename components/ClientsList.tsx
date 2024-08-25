d67cacd55146d5622b365ab1a971d8868a41edaf
Mohamad44p <mohamad44p@yahoo.com>
2024-08-25 11:27:34 +0300

    Edits

--- components/ClientsList.tsx
+++ components/ClientsList.tsx
@@ -1,16 +1,16 @@
 import { Suspense } from "react";
 import dynamic from "next/dynamic";
 import ClientComponent from "./ClientComponent";
-import { unstable_noStore  as noStore} from "next/cache";
-
+import { unstable_noStore as noStore } from "next/cache";
+import db from "@/db/db";
 
 export default async function ClientsPage() {
-
+  const clients = await db.client.findMany();
   return (
     <div className="container py-24 mx-auto px-4 bg-[#faf7f2] overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <Suspense fallback={<div>Loading...</div>}>
-         {/*  <ClientComponent clients={clients} /> */}
+          <ClientComponent clients={clients} />
         </Suspense>
       </div>
     </div>
