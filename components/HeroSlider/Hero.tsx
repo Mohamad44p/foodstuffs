cd20000e94ff482272f0473f4d1c6ba560fd9a53
Mohamad44p <mohamad44p@yahoo.com>
2024-10-17 09:32:04 +0300

    Edit Hero Image

--- components/HeroSlider/Hero.tsx
+++ components/HeroSlider/Hero.tsx
@@ -14,23 +14,26 @@ export default function HeroSection() {
   const videoContainerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className="relative bg-background text-white h-[120vh] overflow-hidden">
+    <div className="relative bg-background text-white h-screen overflow-hidden">
       <motion.div ref={videoContainerRef} className="absolute inset-0">
         <Image
           className="h-full w-full object-cover"
           alt="Adeeb Juneidi Food Products"
-          src="/HeroImage.jpg"
+          src="/HeroBanner3.jpg"
           width={1920}
           height={1080}
         />
       </motion.div>
+      <div
+        className="absolute inset-0 bg-black/50"
+      />
       <motion.div
         className="absolute left-4 top-0 bottom-0 flex items-center"
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8 }}
       >
-        <Card className="bg-black/50 backdrop-blur-md border-none shadow-xl max-w-[300px] sm:max-w-[350px] md:max-w-[400px] lg:max-w-[450px]">
+        <Card className="bg-black/50 backdrop-blur-md border-none shadow-xl max-w-[300px] sm:max-w-[350px] md:max-w-[300px] lg:max-w-[550px] mt-[8rem]">
           <CardContent className="p-4 sm:p-6 space-y-4 sm:space-y-6">
             <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white leading-tight">
               {t("title")}
